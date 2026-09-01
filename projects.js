baf6663a09c5ef4d766497abedffc87571251e8c
Kris <46921047+ToomKris@users.noreply.github.com>
2026-09-01 16:19:35 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,1 +1,59 @@
+```javascript
+const projects = [
+
+  {
+    ID:
+    name: "API Tester",
+    description: "A simple API testing tool.",
+    function: "personal",
+    tags: ["HTML", "CSS", "JavaScript"],
+    link: "#"
+  },
+
+  {
+    ID: "1"
+    name: "Website as a School Project",
+    description: "A group project made for the course LTAT.03.015",
+    function: "school",
+      tags: ["HTML", "CSS", "JavaScript"],
+    link: "https://helenleikmaa.github.io/veeb_projekt/"
+  },
+ 
+  {
+    ID: "2"
+    name: "JavaScript-Based Web Development",
+    description: "It is a homework for the University of Tartu subject Websites for advanced students. The homework emphasized the use of JavaScript.",
+    function: "school",
+      tags: ["HTML", "CSS", "JavaScript"],
+    link: "https://toomkris.github.io/Interactive-web-page/"
+  },
+
+ {
+    ID: "3"
+    name: "Website as Cookbook",
+    description: "It is a homework for the University of Tartu subject Websites for advanced students. The homework was to create a cookbook for the restaurant.",
+    function: "school",
+      tags: ["HTML", "CSS"],
+    link: "https://toomkris.github.io/Website-as-Cookbook/#"
+  },
+  {
+    ID: "5"
+    name: "The Soaps website with Bootstrap elements",
+    description: "It is a homework for the University of Tartu subject Websites for advanced students. The homework emphasized the use of JBootstrap",
+    function: "school",
+      tags: ["HTML", "CSS"],
+    link: "https://toomkris.github.io/The-Soaps-website-Bootstrap-elements/"
+  },
+
+  {
+    ID: "4"
+    name: "Coding Challenge",
+    description: "A programming challenge I completed.",
+    function: "challenges",
+    tags: ["Python"],
+    link: "#"
+  }
+
+];
+```
 
